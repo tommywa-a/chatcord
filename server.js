b6c0bc3ae027734974f41b77783d4bf851fd18f6
tommywa-a <tomiwa95@gmail.com>
2023-04-14 14:53:24 +0100

    Setting up socket.io

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const path = require('path')
 const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
+const io = socketio(server)
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
+
+// Run when a client connects
+oninput.on('connection', socket => {
+  console.log('New WS Connection...')
+})
 
 const PORT = 3000 || process.env.PORT
 
