5e67f747e439045757163e111742802575d125d6
tommywa-a <tomiwa95@gmail.com>
2023-04-17 09:39:25 +0100

    Emitting connection and disconnection messages

--- server.js
+++ server.js
@@ -12,9 +12,16 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 // Run when a client connects
 io.on('connection', socket => {
-  console.log('New WS Connection...')
-
+  // Welcome current user
   socket.emit('message', 'Welcome to Chatcord!')
+
+  // Broadcast when a user connects
+  socket.broadcast.emit('message', 'A user has joined the chat')
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat')
+  })
 })
 
 const PORT = 3000 || process.env.PORT
