deab14749026bc73848f94bbff5a0a30607c53a8
tommywa-a <tomiwa95@gmail.com>
2023-04-20 14:17:39 +0100

    Modified server.js

--- server.js
+++ server.js
@@ -15,28 +15,33 @@ app.use(express.static(path.join(__dirname, 'public')))
 const botName = 'ChatCord Bot'
 
 // Run when a client connects
-io.on('connection', socket => {
-  socket.on('joinRoom', ({username, room}) => {
-    const user = userJoin(socket.id, username, room)
+io.on('connection', (socket) => {
+	socket.on('joinRoom', ({ username, room }) => {
+		const user = userJoin(socket.id, username, room)
 
-    socket.join(user.room)
+		socket.join(user.room)
 
-    // Welcome current user
-    socket.emit('message', formatMessage(botName, 'Welcome to Chatcord!'))
-  
-    // Broadcast when a user connects
-    socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`))
-  })
-  
-  // Listen for chatMessage
-  socket.on('chatMessage', (msg) => {
-    io.emit('message', formatMessage('USER', msg))
-  })
+		// Welcome current user
+		socket.emit('message', formatMessage(botName, 'Welcome to Chatcord!'))
 
-    // Runs when client disconnects
-    socket.on('disconnect', () => {
-      io.emit('message', formatMessage(botName, 'A user has left the chat'))
-    })
+		// Broadcast when a user connects
+		socket.broadcast
+			.to(user.room)
+			.emit(
+				'message',
+				formatMessage(botName, `${user.username} has joined the chat`)
+			)
+	})
+
+	// Listen for chatMessage
+	socket.on('chatMessage', (msg) => {
+		io.emit('message', formatMessage('USER', msg))
+	})
+
+	// Runs when client disconnects
+	socket.on('disconnect', () => {
+		io.emit('message', formatMessage(botName, 'A user has left the chat'))
+	})
 })
 
 const PORT = 3000 || process.env.PORT
