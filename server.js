f02c16fb7bf9d4fbfb1eb77c514827308f258fbe
tommywa-a <tomiwa95@gmail.com>
2023-04-21 13:32:33 +0100

    formated server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ io.on('connection', (socket) => {
 
 	// Listen for chatMessage
 	socket.on('chatMessage', (msg) => {
-    const user = getCurrentUser(socket.id)
+		const user = getCurrentUser(socket.id)
 
 		io.to(user.room).emit('message', formatMessage(user.username, msg))
 	})
