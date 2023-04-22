11b6559aa12e45f6066e2e1d5dc893352bf7d188
tommywa-a <tomiwa95@gmail.com>
2023-04-22 23:28:28 +0100

    function for when user leaves chat

--- utils/users.js
+++ utils/users.js
@@ -13,6 +13,15 @@ function getCurrentUser(id) {
 	return users.find((user) => user.id === id)
 }
 
+// User leaves chat
+function userLeave(id) {
+	const index = users.findIndex(user => user.id === id)
+
+	if (index !== -1) {
+		return users.splice(index, 1)
+	}
+}
+
 module.exports = {
 	userJoin,
 	getCurrentUser,
