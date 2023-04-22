50d642257d4ec44a98b38ebb1208744c359c974f
tommywa-a <tomiwa95@gmail.com>
2023-04-22 23:29:35 +0100

    modified users.js

--- utils/users.js
+++ utils/users.js
@@ -15,7 +15,7 @@ function getCurrentUser(id) {
 
 // User leaves chat
 function userLeave(id) {
-	const index = users.findIndex(user => user.id === id)
+	const index = users.findIndex((user) => user.id === id)
 
 	if (index !== -1) {
 		return users.splice(index, 1)
