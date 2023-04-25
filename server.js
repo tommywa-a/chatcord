045430ed93d1a3067ccf0c6d5c426bbecacd9d59
tommywa-a <80416515+tommywa-a@users.noreply.github.com>
2023-04-25 19:37:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 const formatMessage = require('./utils/messages')
-const { userJoin, getCurrentUser } = require('./utils/users')
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users')
 
 const app = express()
 const server = http.createServer(app)
