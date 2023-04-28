30f9daa3f6881026668dcbbfb632d5ff37c4158e
tommywa-a <tomiwa95@gmail.com>
2023-04-28 12:05:52 +0100

    Output room name to chat.html

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 const chatForm = document.getElementById('chat-form')
 const chatMessages = document.querySelector('.chat-messages')
+const roomName = document.getElementById('room-name')
+const userList = document.getElementById('users')
 
 // Get username and room from URL
 const {username, room} = Qs.parse(location.search, {
@@ -52,3 +54,9 @@ function outputMessage(message) {
   </p>`
   document.querySelector('.chat-messages').appendChild(div)
 }
+
+// Add room name to DOM
+function outputRoomName(room) {
+  roomName.innerText = room
+}
+
